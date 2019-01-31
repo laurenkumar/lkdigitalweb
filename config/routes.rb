f3b3dcc01b148ed66f6fe0a5da8f83b6e2668a1d
uthaya_s <sopika16@hotmail.fr>
2019-01-31 11:38:57 +0100

    h1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   root to: 'main#index'
+  root to: 'main#index'
 
   get 'work' => 'pages#work'
   get 'us' => 'pages#us'
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   get 'dorian' => 'pages#dorian'
   get 'sopika' => 'pages#sopika'
   get 'jananika' => 'pages#jananika'
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/422", to: "errors#unacceptable", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 
 end
